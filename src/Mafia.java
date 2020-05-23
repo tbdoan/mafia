b48d7d0c357c3c16ffdaff1c9e1ebacb3a41eb21
vaibhavi8 <vaibjhawar@gmail.com>
2020-05-23 16:25:23 -0700

    Update Mafia.java

--- src/Mafia.java
+++ src/Mafia.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Goon extends Player
+public class Mafia extends Player
 {
-    public Goon(String name, boolean status)
+    public Mafia(String name, boolean status)
     {
         super(name, status);
     }
