15c350c5442648e7dd8ee1f2f22fdc648631765b
vaibhavi8 <vaibjhawar@gmail.com>
2020-05-23 16:22:05 -0700

    Create Mafia.java

---
+++ src/Mafia.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Goon extends Player
+{
+    public Goon(String name, boolean status)
+    {
+        super(name, status);
+    }
+    @SuppressWarnings("resource")
+    public void murder(ArrayList <String> alive,ArrayList<String> dead)
+    {
+        Scanner c = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Who would you like to kill?");
+            String kill = c.nextLine();
+            String temp;
+            for(int n = 0; n < alive.size(); n++)
+            {
+            	for(String i : alive)
+            	{
+            		if(kill.equals(alive.get(n)));
+            		{
+            			temp = alive.get(n);
+            			dead.add(temp);
+            			alive.remove(temp);
+            			break;
+            		}
+            	}
+            }
+            break;
+        }
+    }
+}
