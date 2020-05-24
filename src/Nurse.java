c90852744cf4b50c72a43204ec9bc47f96080dc0
vaibhavi8 <vaibjhawar@gmail.com>
2020-05-23 18:05:36 -0700

    Update Nurse.java

--- src/Nurse.java
+++ src/Nurse.java
@@ -1,38 +1,28 @@
-/*
+package Mafia;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Nurse extends Villager
 {
-
     public Nurse(String name, boolean status)
     {
         super(name, status);
     }
     @SuppressWarnings("resource")
-    public void save(String save, int n, int i, ArrayList <String> alive,ArrayList<String> dead)
+    public Player save(String save, ArrayList <Player> players)
     {
         Scanner c = new Scanner(System.in);
-        while(n=0)
+        while(true)
         {
             System.out.println("Who would you like to save?");
             save = c.nextLine();
-            for(i = 0; i < alive.size(); i++)
+            for(int i = 0; i<players.size();i++)
             {
-                if(save.equals(dead.get(i)))
-                {
-                    alive.add(dead.get(i));
-                    dead.remove(i);
-                    n++;
-                }
-                if(save.equals(alive(i)))
-                {
-                    n++;
-                }
-                if(!(save.equals(dead.get(i))||save.equals(alive.get(i))))
-                {
-                    System.out.println("Error made, please try again.");
-                }
+            	if(save.equals(players.get(i).getName()) && players.get(i).getStatus())
+            	{
+            		return players.get(i);
+            	}
             }
+            System.out.println("Player doesn't exist or is already dead. Try again");
         }
     }
-} */
+}
